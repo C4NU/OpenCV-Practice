351b75e54303d5e1b25dbf6158b845c5a140eea6
C4NU <canu1832@gmail.com>
2023-03-14 17:26:36 +0900

    Initial Commit

---
+++ OpenCV/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  OpenCV
+//
+//  Created by 전효재 on 2023/03/14.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
